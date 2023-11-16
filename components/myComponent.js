10bed1ac9c5fe754b48b6948f0f83eacf0c9d194
DELL <mohamedelbarrak@outlook.fr>
2023-11-16 16:08:41 +0100

    Version 0

--- components/myComponent.js
+++ components/myComponent.js
@@ -14,7 +14,6 @@ class MyBox extends HTMLElement {
     this.attachShadow({ mode: 'open' });
 
     this.shadowRoot.innerHTML = `
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css" rel="stylesheet">
       <div class="box1">
         My Media player
         <br>
